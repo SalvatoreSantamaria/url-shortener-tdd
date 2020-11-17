255240c1927ff8502d9ca9e285af3ac4a20158e9
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-11-16 20:23:08 -0800

    Refactored test with beforeEach, added readme

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-Run a test with npm run test
+The function of this mini project is to practice TDD and complete a small URL shortener. 
+The URL shortener should return a unique, shortened URL with passed a normal URL.
+The URL shortener should also return back the original URL when passed the shortened URL.
+
+Built with Node.js, Mocha, and Chai
+
+Run tests npm run test
--- src/urlProcess.test.js
+++ src/urlProcess.test.js
@@ -2,25 +2,24 @@ import { expect } from 'chai'
 const UrlProcess = require('./urlProcess');
 
 describe('Running the urlProcesser', () => {
-  it ("should run tests", () => {
-    const urlProcess = new UrlProcess()
+
+  let urlProcess;
+  beforeEach(()=> {
+    urlProcess = new UrlProcess
   })
 
   it ("should have a method shortener that should return a URL", () => {
-    const urlProcess = new UrlProcess()
     let actual = urlProcess.shortener('www.test.com')
     expect(actual).to.deep.equal("http://shorturl1")
   })
 
   it ("should return a url that is unique", () => {
-    const urlProcess = new UrlProcess()
     let first = urlProcess.shortener('www.test.com')
     let second = urlProcess.shortener('www.test.com')
     expect(first).to.not.equal(second)
   })
 
   it ("should have a method lengthener that will return the original URL when given the shortened URL", () => {
-    const urlProcess = new UrlProcess()
     urlProcess.shortener('www.reallylongurl.com')
     let lengthen = urlProcess.lengthener("http://shorturl1")
     expect(lengthen).to.equal('www.reallylongurl.com')
